89a51f4012ab607dd92b2336bbb01b2a1b1444bb
Javy011021 <garciaj1246@gmail.com>
2022-06-03 18:40:25 -0400

    Clase Universidad

---
+++ src/Logic/University.java
@@ -0,0 +1,17 @@
+package Logic;
+
+import java.util.ArrayList;
+
+public class University {
+	private ArrayList<Person> staff;
+	private Faculty computerFac;
+	
+	public ArrayList<Person> getStaff() {
+		return staff;
+	}
+
+	public Faculty getComputerFac() {
+		return computerFac;
+	}
+	
+}
