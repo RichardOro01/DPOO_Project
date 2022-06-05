58abc26a96c79a5b1de0ebeb37ada0d70774b376
Javy011021 <garciaj1246@gmail.com>
2022-06-05 16:01:11 -0400

    Clase Estudiante

---
+++ src/Logic/Student.java
@@ -0,0 +1,24 @@
+package Logic;
+
+public class Student extends Person{
+	//attributes
+	private String year;
+	private String group;
+	
+	//getters
+	public String getYear() {
+		return year;
+	}
+	public String getGroup() {
+		return group;
+	}
+	
+	
+	//setters
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+}
