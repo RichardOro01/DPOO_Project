99e8dcf4d08a91ade8cfdc75ad5293180227f5e0
Javy011021 <garciaj1246@gmail.com>
2022-06-05 15:41:06 -0400

    Clase Directivo y Profesor

---
+++ src/Logic/Executive.java
@@ -0,0 +1,15 @@
+package Logic;
+
+public class Executive extends Person{
+	//attributes
+	private String position;
+	private String area;
+	
+	//getters
+	public String getPosition() {
+		return position;
+	}
+	public String getArea() {
+		return area;
+	}
+}
