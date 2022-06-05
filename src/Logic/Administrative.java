dc019251fbb34b474456ae5ee67d74aa2efe7005
Javy011021 <garciaj1246@gmail.com>
2022-06-05 15:49:01 -0400

    Clase Administrativo

---
+++ src/Logic/Administrative.java
@@ -0,0 +1,17 @@
+package Logic;
+
+public class Administrative extends Person {
+	//attributes
+	private String job;
+	
+	//getters
+	public String getJob() {
+		return job;
+	}
+
+		
+	//setters
+	public void setJob(String job) {
+		this.job = job;
+	}
+}
